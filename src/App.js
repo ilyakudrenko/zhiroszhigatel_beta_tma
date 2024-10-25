2043fb18784250d82976290bdc0a1708b1ce5f7d
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2024-10-24 22:41:16 -0700

    Testing the card cell with horizontal scroll

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import {AppRoot, Placeholder, Button, Card} from '@telegram-apps/telegram-ui';
 import {CardChip} from "@telegram-apps/telegram-ui/dist/components/Blocks/Card/components/CardChip/CardChip";
 import {CardCell} from "@telegram-apps/telegram-ui/dist/components/Blocks/Card/components/CardCell/CardCell";
 import React from "react";
+import {
+    HorizontalScroll
+} from "@telegram-apps/telegram-ui/dist/cjs/components/Service/HorizontalScroll/HorizontalScroll";
 
 
 const App = () => (
@@ -23,29 +26,55 @@ const App = () => (
             >
                 Action
             </Button>
-            <Card type="ambient">
-                <React.Fragment key=".0">
-                    <CardChip readOnly>
-                        Hot place
-                    </CardChip>
-                    <img
-                        alt="Dog"
-                        src="https://i.imgur.com/892vhef.jpeg"
-                        style={{
-                            display: 'block',
-                            height: 308,
-                            objectFit: 'cover',
-                            width: 254
-                        }}
-                    />
-                    <CardCell
-                        readOnly
-                        subtitle="United states"
-                    >
-                        New York
-                    </CardCell>
-                </React.Fragment>
-            </Card>
+            <HorizontalScroll>
+                <Card type="ambient">
+                    <React.Fragment key=".0">
+                        <CardChip readOnly>
+                            Hot place
+                        </CardChip>
+                        <img
+                            alt="Dog"
+                            src="https://i.imgur.com/892vhef.jpeg"
+                            style={{
+                                display: 'block',
+                                height: 308,
+                                objectFit: 'cover',
+                                width: 254
+                            }}
+                        />
+                        <CardCell
+                            readOnly
+                            subtitle="United states"
+                        >
+                            New York
+                        </CardCell>
+                    </React.Fragment>
+                </Card>
+                <Card type="ambient">
+                    <React.Fragment key=".0">
+                        <CardChip readOnly>
+                            Hot place
+                        </CardChip>
+                        <img
+                            alt="Dog"
+                            src="https://i.imgur.com/892vhef.jpeg"
+                            style={{
+                                display: 'block',
+                                height: 308,
+                                objectFit: 'cover',
+                                width: 254
+                            }}
+                        />
+                        <CardCell
+                            readOnly
+                            subtitle="United states"
+                        >
+                            New York
+                        </CardCell>
+                    </React.Fragment>
+                </Card>
+            </HorizontalScroll>
+
         </Placeholder>
     </AppRoot>
 );
