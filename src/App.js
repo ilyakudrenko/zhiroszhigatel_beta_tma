98dcf70168cc6544765cdd6c58a89ec12d5f9bf7
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2024-10-28 16:47:58 -0700

    Creating profile Cell - Trying to make the border radius

--- src/App.js
+++ src/App.js
@@ -29,130 +29,128 @@ const roundedCellStyle = {
 const App = () => (
     <AppRoot>
 
+        <List
+            style={{
+                backgroundColor: 'var(--tgui--secondary_bg_color)',
+                width: '100%',
+            }}
+        >
+            <Section
+            >
+                <Cell
+                    after={<Icon24ChevronRight />}
+                    before={<Icon32ProfileColoredSquare />}
+                    style={
+                        roundedCellStyle
+                    }
+                >
+                    Profile
+                </Cell>
+                <Cell
+                    after={<Icon24ChevronRight />}
+                    before={<Icon24Chat />}
+                    style={
+                        roundedCellStyle
+                    }
+                >
+                    Support
+                </Cell>
+            </Section>
+        </List>
+        <Banner
+            background={<img alt="Nasa streams" src="https://www.nasa.gov/wp-content/uploads/2023/10/streams.jpg?resize=1536,864" style={{width: '150%'}}/>}
+            callout="Urgent notification"
+            description="Start exploring TON in a new, better way"
+            header="Introducing TON Space"
+            // onCloseIcon={function noRefCheck(){}}
+            type="section"
+        >
+            <React.Fragment key=".0">
+                <Button size="s">
+                    Try it out
+                </Button>
+            </React.Fragment>
+        </Banner>
 
+        <HorizontalScroll>
+            <div style={{ display: 'flex', overflowX: 'scroll', whiteSpace: 'nowrap', gap: '16px' }}>
+                <Card type="ambient">
+                    <React.Fragment key=".0">
+                        <CardChip readOnly>
+                            Hot place
+                        </CardChip>
+                        <img
+                            alt="Dog"
+                            src="https://i.imgur.com/892vhef.jpeg"
+                            style={{
+                                display: 'block',
+                                height: 308,
+                                objectFit: 'cover',
+                                width: 254
+                            }}
+                        />
+                        <CardCell
+                            readOnly
+                            subtitle="United states"
+                        >
+                            New York
+                        </CardCell>
+                    </React.Fragment>
+                </Card>
+                <Card type="ambient">
+                    <React.Fragment key=".0">
+                        <CardChip readOnly>
+                            Hot place
+                        </CardChip>
+                        <img
+                            alt="Dog"
+                            src="https://i.imgur.com/892vhef.jpeg"
+                            style={{
+                                display: 'block',
+                                height: 308,
+                                objectFit: 'cover',
+                                width: 254
+                            }}
+                        />
+                        <CardCell
+                            readOnly
+                            subtitle="United states"
+                        >
+                            New York
+                        </CardCell>
+                    </React.Fragment>
+                </Card>
+                <Card type="ambient">
+                    <React.Fragment key=".0">
+                        <CardChip readOnly>
+                            Hot place
+                        </CardChip>
+                        <img
+                            alt="Dog"
+                            src="https://i.imgur.com/892vhef.jpeg"
+                            style={{
+                                display: 'block',
+                                height: 308,
+                                objectFit: 'cover',
+                                width: 254
+                            }}
+                        />
+                        <CardCell
+                            readOnly
+                            subtitle="United states"
+                        >
+                            New York
+                        </CardCell>
+                    </React.Fragment>
+                </Card>
+            </div>
+        </HorizontalScroll>
 
         <Placeholder
             header="Title"
             description="App.js test"
         >
-            <List
-                style={{
-                    backgroundColor: 'var(--tgui--secondary_bg_color)',
-                    width: '100%',
-                    borderRadius: '16px',
-                }}
-            >
-                <Section
-                    style={{
-                        borderRadius: '16px',
-                    }}
-                >
-                    <Cell
-                        after={<Icon24ChevronRight />}
-                        before={<Icon32ProfileColoredSquare />}
-                        style={{
-                            roundedCellStyle
-                        }}
-                    >
-                    Profile
-                    </Cell>
-                    <Cell
-                        after={<Icon24ChevronRight />}
-                        before={<Icon24Chat />}
 
-                    >
-                        Support
-                    </Cell>
-                </Section>
-            </List>
-            <Banner
-                background={<img alt="Nasa streams" src="https://www.nasa.gov/wp-content/uploads/2023/10/streams.jpg?resize=1536,864" style={{width: '150%'}}/>}
-                callout="Urgent notification"
-                description="Start exploring TON in a new, better way"
-                header="Introducing TON Space"
-                // onCloseIcon={function noRefCheck(){}}
-                type="section"
-            >
-                <React.Fragment key=".0">
-                    <Button size="s">
-                        Try it out
-                    </Button>
-                </React.Fragment>
-            </Banner>
-
-            <HorizontalScroll>
-                <div style={{ display: 'flex', overflowX: 'scroll', whiteSpace: 'nowrap', gap: '16px' }}>
-                    <Card type="ambient">
-                        <React.Fragment key=".0">
-                            <CardChip readOnly>
-                                Hot place
-                            </CardChip>
-                            <img
-                                alt="Dog"
-                                src="https://i.imgur.com/892vhef.jpeg"
-                                style={{
-                                    display: 'block',
-                                    height: 308,
-                                    objectFit: 'cover',
-                                    width: 254
-                                }}
-                            />
-                            <CardCell
-                                readOnly
-                                subtitle="United states"
-                            >
-                                New York
-                            </CardCell>
-                        </React.Fragment>
-                    </Card>
-                    <Card type="ambient">
-                        <React.Fragment key=".0">
-                            <CardChip readOnly>
-                                Hot place
-                            </CardChip>
-                            <img
-                                alt="Dog"
-                                src="https://i.imgur.com/892vhef.jpeg"
-                                style={{
-                                    display: 'block',
-                                    height: 308,
-                                    objectFit: 'cover',
-                                    width: 254
-                                }}
-                            />
-                            <CardCell
-                                readOnly
-                                subtitle="United states"
-                            >
-                                New York
-                            </CardCell>
-                        </React.Fragment>
-                    </Card>
-                    <Card type="ambient">
-                        <React.Fragment key=".0">
-                            <CardChip readOnly>
-                                Hot place
-                            </CardChip>
-                            <img
-                                alt="Dog"
-                                src="https://i.imgur.com/892vhef.jpeg"
-                                style={{
-                                    display: 'block',
-                                    height: 308,
-                                    objectFit: 'cover',
-                                    width: 254
-                                }}
-                            />
-                            <CardCell
-                                readOnly
-                                subtitle="United states"
-                            >
-                                New York
-                            </CardCell>
-                        </React.Fragment>
-                    </Card>
-                </div>
-            </HorizontalScroll>
 
         </Placeholder>
     </AppRoot>
