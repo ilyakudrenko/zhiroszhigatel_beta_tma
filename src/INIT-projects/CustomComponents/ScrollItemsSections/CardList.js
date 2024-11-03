d9aae18652328de5da2f66c25a7dbb669911fded
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2024-11-02 21:51:55 -0700

    feat: testing modal for the card item

--- src/INIT-projects/CustomComponents/ScrollItemsSections/CardList.js
+++ src/INIT-projects/CustomComponents/ScrollItemsSections/CardList.js
@@ -13,88 +13,38 @@ import {Button, Card, Modal, Placeholder, Text} from "@telegram-apps/telegram-ui
  * @param {string} title - The main title displayed on the card (e.g., location name).
  * @param {string} description - Additional information displayed below the title (e.g., subtitle or country name).
  */
-const INITCardItem = ({ cardChip, imageSrc, title, description }) => {
-    // const navigate = useNavigate();
-
-    // const handleClick = () => {
-    //     navigate('/item');
-    // };
-    return (
-        <Card style={{ flexShrink: 0, minWidth: '254px' }} type="ambient">
-            <CardChip readOnly>
-                {cardChip}
-            </CardChip>
+const INITCardItem = ({ cardChip, imageSrc, title, description }) => (
+    <Modal
+        trigger={
+            <Card style={{ flexShrink: 0, minWidth: '254px' }} type="ambient">
+                <CardChip readOnly>{cardChip}</CardChip>
+                <img
+                    alt={title}
+                    src={imageSrc}
+                    style={{
+                        display: 'block',
+                        height: 308,
+                        objectFit: 'cover',
+                        width: 254
+                    }}
+                />
+                <CardCell readOnly subtitle={description}>{title}</CardCell>
+            </Card>
+        }
+    >
+        <Placeholder description={description} header={title}>
             <img
-                alt={title}
-                src={imageSrc}
+                alt="Telegram sticker"
+                src="https://xelene.me/telegram.gif"
                 style={{
                     display: 'block',
-                    height: 308,
-                    objectFit: 'cover',
-                    width: 254
+                    height: '144px',
+                    width: '144px'
                 }}
             />
-            <CardCell readOnly subtitle={description}>
-                {title}
-            </CardCell>
-        </Card>
-    );
-};
-
-// const INITModalCardItem = ({ cardChip, imageSrc, title, description }) => {
-//     return(
-//       <Modal
-//         trigger={
-//           <INITCardItem
-//               cardChip={cardChip}
-//               imageSrc={imageSrc}
-//               title={title}
-//               description={description}
-//           />
-//         }
-//       >
-//           <Placeholder
-//               description={description}
-//               header={title}
-//           >
-//               <img
-//                   alt="Telegram sticker"
-//                   src="https://xelene.me/telegram.gif"
-//                   style={{
-//                       display: 'block',
-//                       height: '144px',
-//                       width: '144px'
-//                   }}
-//               />
-//           </Placeholder>
-//       </Modal>
-//     );
-// };
-//
-// const INITModalCardItem = ({ cardChip, imageSrc, title, description }) => {
-//     return(
-//       <Modal
-//         trigger={
-//
-//       }
-//       >
-//           <Placeholder
-//               description={description}
-//               header={title}
-//           >
-//               <img
-//                   alt="Telegram sticker"
-//                   src="https://xelene.me/telegram.gif"
-//                   style={{
-//                       display: 'block',
-//                       height: '144px',
-//                       width: '144px'
-//                   }}
-//               />
-//           </Placeholder>
-//       </Modal>
-//     );
-// };
+        </Placeholder>
+    </Modal>
+);
 
 /**
  * Component for displaying a list of card items horizontally.
@@ -107,26 +57,6 @@ const INITCardItem = ({ cardChip, imageSrc, title, description }) => {
  *   - description: Subtitle or additional information for the card
  */
 const INITCardsList = ({ items = [] }) => {
-    // const [isModalOpen, setIsModalOpen] = useState(false);
-    // const [selectedItem, setSelectedItem] = useState(null);
-
-    // const openModal = (item) => {
-    //     setSelectedItem(item);
-    //     setIsModalOpen(true);
-    // };
-
-    // const closeModal = () => {
-    //     setIsModalOpen(false);
-    //     setSelectedItem(null); // Reset selected item
-    // };
-
-    // const navigate = useNavigate();
-
-    // const handleBuyClick = () => {
-    //     closeModal(); // Close modal first
-    //     navigate('/item'); // Then navigate to the item page
-    // };
-
     return (
         <div>
             <div
@@ -148,41 +78,6 @@ const INITCardsList = ({ items = [] }) => {
                     />
                 ))}
             </div>
-
-            {/*{isModalOpen && (*/}
-            {/*    <Modal open={isModalOpen} onClose={closeModal}>*/}
-            {/*        /!* Custom Modal Header with Close Button *!/*/}
-            {/*        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px' }}>*/}
-            {/*            <h3 style={{ margin: 0 }}></h3>*/}
-            {/*            <IconButton onClick={closeModal}>*/}
-            {/*                <Icon28Close style={{ color: 'var(--tgui--plain_foreground)' }} />*/}
-            {/*            </IconButton>*/}
-            {/*        </div>*/}
-
-            {/*        <Placeholder*/}
-            {/*            description={selectedItem?.description}*/}
-            {/*            header={selectedItem?.title}*/}
-            {/*        >*/}
-            {/*            <img*/}
-            {/*                alt="Telegram sticker"*/}
-            {/*                src="https://xelene.me/telegram.gif"*/}
-            {/*                style={{*/}
-            {/*                    display: 'block',*/}
-            {/*                    height: '144px',*/}
-            {/*                    width: '144px',*/}
-            {/*                }}*/}
-            {/*            />*/}
-            {/*            <Button*/}
-            {/*                mode="filled"*/}
-            {/*                size="s"*/}
-            {/*                stretched*/}
-            {/*                onClick={handleBuyClick} // Ensure Buy button also closes modal*/}
-            {/*            >*/}
-            {/*                Buy*/}
-            {/*            </Button>*/}
-            {/*        </Placeholder>*/}
-            {/*    </Modal>*/}
-            {/*)}*/}
         </div>
     );
 };
