24a375dc84485e74d739e5a776f7ca553d50d701
ilyakudrenko <ilya.kudrenko@gmail.com>
2024-11-08 13:50:24 -0600

    test

--- src/INIT-projects/HomePage/HomePage.js
+++ src/INIT-projects/HomePage/HomePage.js
@@ -10,15 +10,43 @@ import {
 
 } from '@telegram-apps/telegram-ui';
 import React from "react";
+import {Icon24ChevronRight} from "@telegram-apps/telegram-ui/dist/icons/24/chevron_right";
 import {HorizontalScroll} from "@telegram-apps/telegram-ui/dist/components/Service/HorizontalScroll/HorizontalScroll";
 import INITCardsList from "../CustomComponents/ScrollItemsSections/CardList";
 import INITDivider from "../CustomComponents/Dividers/Divider";
+import INITMessageBadgeIcon from "../CustomComponents/Icons/MessageBadgeIcon";
+import INITProfileIcon from "../CustomComponents/Icons/ProfileIcon";
 import INITBanner from "../CustomComponents/Banner/Banner";
-import Navigation from "../CustomComponents/Navigation/Navigation";
+
+const roundedCellStyle = {
+    borderRadius: '16px',
+    overflow: 'hidden', // Ensures rounded corners display properly
+};
+
+const handleClick = () => {
+    alert("Button clicked!");
+};
 
 const HomePage = ({ data }) => (
     <AppRoot>
-        <Navigation />
+        <List>
+            <Section
+                style={roundedCellStyle}
+            >
+                <Cell
+                    after={<Icon24ChevronRight />}
+                    before={<INITProfileIcon />}
+                >
+                    Profile
+                </Cell>
+                <Cell
+                    after={<Icon24ChevronRight />}
+                    before={<INITMessageBadgeIcon color="white" />}
+                >
+                    Support
+                </Cell>
+            </Section>
+        </List>
 
         {/*Some banner for sales*/}
         <INITDivider color = 'transparent' thickness="10%" />
