a5da5e2d3fe400cc6e0ac7629653893a971469a1
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2024-11-08 18:39:05 -0800

    Testing Guide 1

--- src/INIT-projects/Zhiroszhigatel/CustomGuides/Guide_1/Guide1.js
+++ src/INIT-projects/Zhiroszhigatel/CustomGuides/Guide_1/Guide1.js
@@ -1,55 +1,56 @@
 import React from 'react';
-import {Image, List, Section, Title, Text, } from "@telegram-apps/telegram-ui";
+import {Image, List, Section, Title, Text, Cell,} from "@telegram-apps/telegram-ui";
 import logo from "./Images/Logo.jpg";
 // import logo from "./Images/Logo.jpg";
+
+const roundedCellStyle = {
+    borderRadius: '16px',
+    overflow: 'hidden', // Ensures rounded corners display properly
+};
+
 const INITGuide_1 = ({info = [],  }) => {
     return (
         <List>
             <Image
-                src={"./Images/Logo.jpg"}
+                src={"./Images/Loxgo.jpg"}
             />
-            <Section>
-                <Title
-                    level="1"
+            <Section
+                style={roundedCellStyle}
+            >
+                <Text
                     weight="1"
                 >
                     Пояснение
-                </Title>
-
-                <Text weight="3">
-                    Здесь будут группы
-                    продуктов и мои
-                    пояснения к ним.
                 </Text>
+                <Cell>
+                    <Text weight="3">
+                        Здесь будут группы
+                        продуктов и мои
+                        пояснения к ним.
+                    </Text>
+                </Cell>
+                <Cell>
+                    <Text weight="3">
+                        Наиболее оптимальные
+                        продукты для питания,
+                        то есть энергетически не
 
-                <Text weight="3">
-                    Наиболее оптимальные
-                    продукты для питания,
-                    то есть энергетически не
+                        плотные и богатые содер-
+                        жанию витаминов, минералов (содержат отно-
+                        сительно немного калорий на 100гр продукта).
+                    </Text>
+                </Cell>
+                <Cell>
+                    <Text weight="3">
+                        Выбор таких продуктов, хорошая стратегия
+                        для поддержания оптимального здоровья,
 
-                    плотные и богатые содер-
-                    жанию витаминов, минералов (содержат отно-
-                    сительно немного калорий на 100гр продукта).
-                </Text>
+                        самочувствия, а главное поможет в построе-
+                        нии или поддержании здорового подтянутого
 
-                <Text weight="3">
-                    Выбор таких продуктов, хорошая стратегия
-                    для поддержания оптимального здоровья,
-
-                    самочувствия, а главное поможет в построе-
-                    нии или поддержании здорового подтянутого
-
-                    тела, без лишних килограмм.
-                </Text>
-
-                <Text weight="3">
-
-                </Text>
-
-                <Text weight="3">
-
-                </Text>
-
+                        тела, без лишних килограмм.
+                    </Text>
+                </Cell>
             </Section>
         </List>
     );
