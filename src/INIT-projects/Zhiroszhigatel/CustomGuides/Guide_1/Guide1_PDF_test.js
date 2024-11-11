78759db6a27c7e62e8a3ae9a4bfe4a6be944d3e1
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2024-11-11 14:10:19 -0800

    Testing the PDF in the iframe

--- src/INIT-projects/Zhiroszhigatel/CustomGuides/Guide_1/Guide1_PDF_test.js
+++ src/INIT-projects/Zhiroszhigatel/CustomGuides/Guide_1/Guide1_PDF_test.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import {List} from "@telegram-apps/telegram-ui";
-import pdfFile from './guide_1_pdf_version.pdf'
+// import { Document, Page } from 'react-pdf';
+import {Image, List} from "@telegram-apps/telegram-ui";
+import pdfFile from "./guide_1_pdf_version.pdf"
 
-import { Document, Page, pdfjs } from 'react-pdf';
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
-const PdfViewer = ({ pdfUrl }) => (
-    <Document file={pdfUrl}>
-        <Page pageNumber={1} />
-    </Document>
-);
-
+// Use require to import the PDF file
 
 const INITGuide_1_PDF = () => {
     return (
         <List>
-            {/*<PdfViewer pdfUrl={"https://drive.google.com/file/d/1dGXOJgnDkjSaq-1MpLFJeK0woT73A43K/view?usp=sharing"}/>*/}
-            <PdfViewer pdfUrl={pdfFile}/>
+            <object data="http://africau.edu/images/default/sample.pdf" type="application/pdf" width="100%"
+                    height="100%">
+                <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the
+                    PDF!</a></p>
+            </object>
         </List>
-    )
-}
+    );
+};
 
 export default INITGuide_1_PDF;
