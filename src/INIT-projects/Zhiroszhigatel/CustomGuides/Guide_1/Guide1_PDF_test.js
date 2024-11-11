261d0fcb25cb6d6ce21819f1e5504324bbea4fe2
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2024-11-11 15:03:50 -0800

    Testing the PDF in the iframe

--- src/INIT-projects/Zhiroszhigatel/CustomGuides/Guide_1/Guide1_PDF_test.js
+++ src/INIT-projects/Zhiroszhigatel/CustomGuides/Guide_1/Guide1_PDF_test.js
@@ -5,13 +5,19 @@ import pdfUrl from './guide_1_pdf_version.pdf'
 const INITGuide_1_PDF = () => {
     return (
         <div style={{width: '100%', height: '100vh'}}>
-            <iframe
-                src="https://www.escaux.com/rsrc/CustomerDocs/DRD_T38Support_AdminGuide/T38_TEST_PAGES.pdf"  // Replace with the relative path to your PDF file
-                title="PDF Viewer"
+            {/*<iframe*/}
+            {/*    src="https://www.escaux.com/rsrc/CustomerDocs/DRD_T38Support_AdminGuide/T38_TEST_PAGES.pdf"  // Replace with the relative path to your PDF file*/}
+            {/*    title="PDF Viewer"*/}
+            {/*    width="100%"*/}
+            {/*    height="100%"*/}
+            {/*    style={{border: 'none'}}*/}
+            {/*/>*/}
+
+            <embed
+                src="https://www.escaux.com/rsrc/CustomerDocs/DRD_T38Support_AdminGuide/T38_TEST_PAGES.pdf"
                 width="100%"
                 height="100%"
-                style={{border: 'none'}}
-            />
+                type="application/pdf"/>
         </div>
     );
 };
