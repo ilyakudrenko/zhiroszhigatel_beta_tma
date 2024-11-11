3280d3fd881550c5e6747f5ad3ac47362104a203
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2024-11-11 12:58:46 -0800

    Testing the PDF in the iframe

--- src/INIT-projects/Zhiroszhigatel/CustomGuides/Guide_1/Guide1_PDF_test.js
+++ src/INIT-projects/Zhiroszhigatel/CustomGuides/Guide_1/Guide1_PDF_test.js
@@ -2,23 +2,22 @@ import React from 'react';
 import {List} from "@telegram-apps/telegram-ui";
 import pdfFile from './guide_1_pdf_version.pdf'
 
+import { Document, Page, pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 const PdfViewer = ({ pdfUrl }) => (
-    <iframe
-        src={pdfUrl}
-        style={{
-            width: '100%',       // Полная ширина
-            height: '100vh',      // Высота экрана
-            border: 'none',       // Без рамок
-        }}
-        title="PDF Viewer"
-    />
+    <Document file={pdfUrl}>
+        <Page pageNumber={1} />
+    </Document>
 );
 
 
 const INITGuide_1_PDF = () => {
     return (
         <List>
-            <PdfViewer pdfUrl={"https://drive.google.com/file/d/1dGXOJgnDkjSaq-1MpLFJeK0woT73A43K/view?usp=sharing"}/>
+            {/*<PdfViewer pdfUrl={"https://drive.google.com/file/d/1dGXOJgnDkjSaq-1MpLFJeK0woT73A43K/view?usp=sharing"}/>*/}
+            <PdfViewer pdfUrl={pdfFile}/>
         </List>
     )
 }
