fd9fd4ffe41b16a2a62c9bcb7ebad6b444385ae4
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2024-11-11 14:50:44 -0800

    Testing the PDF in the iframe

--- src/INIT-projects/Zhiroszhigatel/CustomGuides/Guide_1/Guide1_PDF_test.js
+++ src/INIT-projects/Zhiroszhigatel/CustomGuides/Guide_1/Guide1_PDF_test.js
@@ -1,33 +1,18 @@
-import React, {useState} from 'react';
-// import { Document, Page } from 'react-pdf';
-import {Image, List} from "@telegram-apps/telegram-ui";
-import pdfFile from "./guide_1_pdf_version.pdf"
-import { Document, Page,pdfjs } from 'react-pdf';
+import React, from 'react';
+import {List} from "@telegram-apps/telegram-ui";
 
-const url = "https://www.escaux.com/rsrc/CustomerDocs/DRD_T38Support_AdminGuide/T38_TEST_PAGES.pdf"
-
-// Use require to import the PDF file
 
 const INITGuide_1_PDF = () => {
-    pdfjs.GlobalWorkerOptions.workerSrc =
-        `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
-
-    function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(numPages);
-        setPageNumber(1);
-    }
-
     return (
-        <List>
-            <Document
-                file={url}
-                onLoadSuccess={onDocumentLoadSuccess}
-            >
-                <Page pageNumber={pageNumber} />
-            </Document>
-        </List>
+        <div style={{width: '100%', height: '100vh'}}>
+            <iframe
+                src='guide_1_pdf_version.pdf'  // Replace with the relative path to your PDF file
+                title="PDF Viewer"
+                width="100%"
+                height="100%"
+                style={{border: 'none'}}
+            />
+        </div>
     );
 };
 
