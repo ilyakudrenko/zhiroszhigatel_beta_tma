f3bf85ae88b4d15d833e66847ab39cea4c0be4e7
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2024-11-11 14:56:00 -0800

    Testing the PDF in the iframe

--- src/INIT-projects/Zhiroszhigatel/CustomGuides/Guide_1/Guide1_PDF_test.js
+++ src/INIT-projects/Zhiroszhigatel/CustomGuides/Guide_1/Guide1_PDF_test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {List} from "@telegram-apps/telegram-ui";
-
+import pdfUrl from './guide_1_pdf_version.pdf'
 
 const INITGuide_1_PDF = () => {
     return (
         <div style={{width: '100%', height: '100vh'}}>
             <iframe
-                src='guide_1_pdf_version.pdf'  // Replace with the relative path to your PDF file
+                src="https://www.escaux.com/rsrc/CustomerDocs/DRD_T38Support_AdminGuide/T38_TEST_PAGES.pdf"  // Replace with the relative path to your PDF file
                 title="PDF Viewer"
                 width="100%"
                 height="100%"
