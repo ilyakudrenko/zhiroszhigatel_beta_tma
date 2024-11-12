7f2a86d6eeffe98a47ce740ad2d97c88aaaf79c6
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2024-11-12 11:42:32 -0800

    Uploading other courses

--- src/INIT-projects/Zhiroszhigatel/CustomGuides/GuideTemplate.js
+++ src/INIT-projects/Zhiroszhigatel/CustomGuides/GuideTemplate.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
-
+/**
+ * INITGuideTemplate Component
+ *
+ * This component dynamically generates a list of images for a guide based on the
+ * total number of pages specified. It expects images to be organized in a folder
+ * structure that matches the guide key and loads them using `require`.
+ *
+ * @param {string} guideKey - Unique identifier for the guide, used to locate the specific image directory.
+ * @param {number} totalPages - Total number of pages (images) in the guide to display.
+ *
+ * @returns {JSX.Element} A list of images that represent each page of the guide.
+ */
 const INITGuideTemplate = ({guideKey, totalPages}) => {
     const guideImages = Array.from({length: totalPages}, (_, index) => {
         // return require(`./Images/Guide_1_page_${index + 1}.jpg`);
