f87646e0538176913e722d212916b66c1707f2d8
ilyakudrenko <ilya.kudrenko@gmail.com>
2024-11-11 21:06:00 -0600

    Testing Guide 1 content

--- src/INIT-projects/Zhiroszhigatel/CustomGuides/Guide_1/Guide1_PDF_test.js
+++ src/INIT-projects/Zhiroszhigatel/CustomGuides/Guide_1/Guide1_PDF_test.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Title} from "@telegram-apps/telegram-ui";
+import INITDivider from "../../../CustomComponents/Dividers/Divider";
 
 // Define the total number of pages in the guide
 const totalPages = 12; // Set this to the actual number of images in your folder
@@ -8,9 +10,41 @@ const guideImages = Array.from({ length: totalPages }, (_, index) => {
     return require(`./Images/Guide_1_page_${index+1}.jpg`);
 });
 
+const containerStyle = {
+    width: "100%",       // Занимает всю возможную ширину
+    height: "500px",     // Фиксированная высота; можно изменить при необходимости
+    overflow: "hidden",  // Обрезает изображение, чтобы соответствовать контейнеру
+    position: "relative",
+    borderRadius: '16px',
+    marginTop: '3%',
+};
+
+const imageStyle = {
+    width: "100%",
+    height: "100%",
+    // objectFit: "cover",  // Масштабирует изображение, сохраняя пропорции, заполняя контейнер
+    position: "absolute",
+    top: 0,
+    left: 0,
+};
+
 const INITGuide_1_PDF = () => {
     return (
         <div>
+            <div style={containerStyle}>
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/3/32/20190616154621%21Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg"
+                    alt="Background"
+                    style={imageStyle}
+                />
+            </div>
+
+            <Title level="1" weight="bold">
+                Гайд по продуктовой корзине
+            </Title>
+
+            <INITDivider color='transparent' thickness="20%"/>
+
             {guideImages.map((src, index) => (
                 <img
                     key={index}
