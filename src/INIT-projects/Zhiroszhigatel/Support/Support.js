19cf0f945b16e84d15d03fa31de7c849201c7035
ilyakudrenko <ilya.kudrenko@gmail.com>
2024-11-13 13:07:06 -0600

    testing Accordion 3

--- src/INIT-projects/Zhiroszhigatel/Support/Support.js
+++ src/INIT-projects/Zhiroszhigatel/Support/Support.js
@@ -4,6 +4,7 @@ import {useNavigate} from "react-router-dom";
 import {SectionHeader} from "@telegram-apps/telegram-ui/dist/components/Blocks/Section/components/SectionHeader/SectionHeader";
 import {AccordionSummary} from "@telegram-apps/telegram-ui/dist/components/Blocks/Accordion/components/AccordionSummary/AccordionSummary";
 import {AccordionContent} from "@telegram-apps/telegram-ui/dist/components/Blocks/Accordion/components/AccordionContent/AccordionContent";
+import INITDivider from "../../CustomComponents/Dividers/Divider";
 
 const roundedCellStyle = {
     borderRadius: '16px',
@@ -22,20 +23,20 @@ const Support = () => {
                     size="s"
                     onClick={() => navigate("/")}
                 >
-                    Back
+                    Назад
                 </Button>
             </SectionHeader>
 
             <Section
                 style={roundedCellStyle}
-                header="Questions"
+                header="Вопросы по курсу"
             >
                 <Accordion
-                    expanded
+                    expanded={true}
                     onChange={function noRefCheck(){}}
                 >
                     <AccordionSummary>
-                        History of accordion
+                        Когда курс и как записаться?
                     </AccordionSummary>
                     <AccordionContent>
                         <div
@@ -44,7 +45,80 @@ const Support = () => {
                             }}
                         >
                             <Blockquote>
-                                The accordion's basic form is believed to have been invented in Berlin, in 1822, by Christian Friedrich Ludwig Buschmann, although one instrument was discovered in 2006 that appears to have been built earlier. The earliest history of the accordion in Russia is poorly documented.
+                                Старт курса в январе 2025!
+                                Сейчас, ты можешь заполнить заявку на предзапись, чтобы потом для тебя были наиболее выгодные условия 💪
+                                <br/>
+                                <a href='https://t.me/+z95IeBYsKZg3MGIy'>https://t.me/+z95IeBYsKZg3MGIy'</a>
+
+                            </Blockquote>
+                        </div>
+                    </AccordionContent>
+                </Accordion>
+
+                <Accordion
+                    expanded={false}
+                    onChange={function noRefCheck(){}}
+                >
+                    <AccordionSummary>
+                        Я смогу похудеть, если я никогда не тренировался?
+                    </AccordionSummary>
+                    <AccordionContent>
+                        <div
+                            style={{
+                                padding: '10px 20px 20px'
+                            }}
+                        >
+                            <Blockquote>
+                                Сможешь, я даю подробные инструкции и видеоматериал для того, чтобы без определённых навыков выполнять все мои рекомендации для похудения
+                            </Blockquote>
+                        </div>
+                    </AccordionContent>
+                </Accordion>
+            </Section>
+
+            <INITDivider color = 'transparent' thickness="10%" />
+
+            <Section
+                style={roundedCellStyle}
+                header="Вопросы по похудению"
+            >
+                <Accordion
+                    expanded={false}
+                    onChange={function noRefCheck(){}}
+                >
+                    <AccordionSummary>
+                        У меня замедлился метаболизм, что делать?
+                    </AccordionSummary>
+                    <AccordionContent>
+                        <div
+                            style={{
+                                padding: '10px 20px 20px'
+                            }}
+                        >
+                            <Blockquote>
+                                Есть только один эффективный способ раскрутить своей метаболизм, это больше двигаться. Шаги – самый простой и самый измеряемый, туда же любая активность в том числе бытовая и запланированная. Замедление метаболизма внутри организма не происходит, это снижение активности и трат из-за образа жизни
+                            </Blockquote>
+                        </div>
+                    </AccordionContent>
+                </Accordion>
+
+                <Accordion
+                    expanded={false}
+                    onChange={function noRefCheck(){}}
+                >
+                    <AccordionSummary>
+                        У меня гипотиреоз, как худеть?
+                    </AccordionSummary>
+                    <AccordionContent>
+                        <div
+                            style={{
+                                padding: '10px 20px 20px'
+                            }}
+                        >
+                            <Blockquote>
+                                Поставленный диагноз гипотиреоза на сегодняшний день, компенсируется приемом гормонами щитовидной железы, назначаемыми вашим лечащим врачом и худеется ровно так же, как без гипотиреоза, подробнее об этом в выпуске с эндокринологом на ютубе
+                                <br/>
+                                <a href='https://www.youtube.com/watch?v=sUnjR0D3Iko'>https://www.youtube.com/watch?v=sUnjR0D3Iko</a>
                             </Blockquote>
                         </div>
                     </AccordionContent>
