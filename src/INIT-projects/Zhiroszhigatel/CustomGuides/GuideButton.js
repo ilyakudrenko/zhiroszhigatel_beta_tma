f12588506e25a5d6463abc117c0ebe1697bcfac5
ilyakudrenko <ilya.kudrenko@gmail.com>
2024-11-14 16:01:53 -0600

    fixing GuideButton

--- src/INIT-projects/Zhiroszhigatel/CustomGuides/GuideButton.js
+++ src/INIT-projects/Zhiroszhigatel/CustomGuides/GuideButton.js
@@ -5,18 +5,19 @@ const GuideButton = () => {
     const [isGreen, setIsGreen] = useState(false);
 
     const handleButtonClick = () => {
-        setIsGreen(!isGreen);
+        setIsGreen(!isGreen); // Toggle the color state
     };
 
     return (
         <div style={{
             position: 'fixed',
             bottom: 0,
+            left: 0,
             width: '100%',
             display: 'flex',
             justifyContent: 'center',
             padding: '20px',
-            zIndex: 1000,
+            zIndex: 1000, // Ensure it’s on top of other elements
         }}>
             <Button
                 mode="filled"
@@ -24,9 +25,8 @@ const GuideButton = () => {
                 onClick={handleButtonClick}
                 style={{
                     backgroundColor: isGreen ? '#53E651' : '',
-                    padding: '12px 24px',
-                    // width: '100%',
-                    // maxWidth: '400px',
+                    // padding: '12px 24px', // Adds more padding to make the button bigger
+                    //marginRight: '8%',
                 }}
             >
                 Добавить в библиотеку
