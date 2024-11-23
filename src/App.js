0eaf001d835b1a3cb5f16b7373bdbcb435d42e26
ilyakudrenko <ilya.kudrenko@gmail.com>
2024-11-22 22:39:48 -0600

    accordion update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import '@telegram-apps/telegram-ui/dist/styles.css';
-import React, {useEffect} from "react";
+import React from "react";
 import {Route, Routes, useLocation} from "react-router-dom";
 import HomePage from "./INIT-projects/HomePage/HomePage";
 import ItemCoursePromo from "./INIT-projects/Zhiroszhigatel/ItemCourse/ItemCoursePromo";
 import Support from "./INIT-projects/Zhiroszhigatel/Support/Support";
 import Profile from "./INIT-projects/Zhiroszhigatel/Profile/Profile";
 import HomePage_StablVersion from "./INIT-projects/HomePage/HomePage_StablVersion";
+
 const App = () => {
     const location = useLocation();
     const isHomePage = location.pathname === "/";
 
-    useEffect(() => {
-        const tg = window.Telegram.WebApp;
-        tg.ready();
-    }, []);
-
     return (
         isHomePage ? (
                 <Routes>
-                    <Route path="/" element={<HomePage_StablVersion/>} />
+                    <Route path="/" element={<HomePage/>} />
                     <Route path="/item" element={<ItemCoursePromo />} />
                     <Route path="/support" element={<Support />} />
                     <Route path="/profile" element={<Profile />} />
--- src/INIT-projects/HomePage/HomePage_StablVersion.js
+++ src/INIT-projects/HomePage/HomePage_StablVersion.js
@@ -35,9 +35,11 @@ const HomePage_StablVersion = () => {
                 </AccordionSummary>
                 <AccordionContent>
                     <div style={{padding: '10px 20px 20px'}}>
+                        <Section>
                         <Blockquote>
                             The accordion's basic form is believed to have been invented in Berlin, in 1822, by Christian Friedrich Ludwig Buschmann, although one instrument was discovered in 2006 that appears to have been built earlier. The earliest history of the accordion in Russia is poorly documented.
                         </Blockquote>
+                        </Section>
                     </div>
                 </AccordionContent>
             </Accordion>
