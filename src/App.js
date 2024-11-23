1c1a0085a16bedf6cb8c74e17f78c05bcbe96810
ilyakudrenko <ilya.kudrenko@gmail.com>
2024-11-22 20:24:31 -0600

    testing profile getting user data

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import HomePage from "./INIT-projects/HomePage/HomePage";
 import ItemCoursePromo from "./INIT-projects/Zhiroszhigatel/ItemCourse/ItemCoursePromo";
 import Support from "./INIT-projects/Zhiroszhigatel/Support/Support";
 import Profile from "./INIT-projects/Zhiroszhigatel/Profile/Profile";
-
 const App = () => {
     const location = useLocation();
     const isHomePage = location.pathname === "/";
 
     return (
         isHomePage ? (
-
                 <Routes>
                     <Route path="/" element={<HomePage/>} />
                     <Route path="/item" element={<ItemCoursePromo />} />
--- src/INIT-projects/Zhiroszhigatel/Profile/Profile.js
+++ src/INIT-projects/Zhiroszhigatel/Profile/Profile.js
@@ -2,12 +2,9 @@ import React from 'react';
 import {AppRoot, Button, Input, Section} from "@telegram-apps/telegram-ui";
 import {useNavigate} from "react-router-dom";
 import INITDivider from "../../CustomComponents/Dividers/Divider";
+// import tg from "https://telegram.org/js/telegram-web-app.js?56";
 
-const roundedCellStyle = {
-    borderRadius: '16px',
-    overflow: 'hidden', // Ensures rounded corners display properly
-    backgroundColor: 'var(--tgui--secondary_bg_color)',
-};
+
 
 const Profile = () => {
     const navigate = useNavigate();
@@ -25,50 +22,7 @@ const Profile = () => {
                     Назад
                 </Button>
             </div>
-            <span>{tg.iniDataUnafe?.user?.username}</span>
-
-            {/*<div style={{display: "flex", justifyContent: "center", alignItems: "center", marginTop: "20px"}}>*/}
-            {/*    <iframe*/}
-            {/*        width="996"*/}
-            {/*        height="560"*/}
-            {/*        src="https://www.youtube.com/embed/POb02mjj2zE"*/}
-            {/*        title="Юность в сапогах (Gachi Version) | Гачимучи ремикс"*/}
-            {/*        frameBorder="0"*/}
-            {/*        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"*/}
-            {/*        referrerPolicy="strict-origin-when-cross-origin"*/}
-            {/*        allowFullScreen*/}
-            {/*        style={{*/}
-            {/*            borderRadius: "10px", // Optional: Add rounded corners*/}
-            {/*            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)", // Optional: Add shadow for better aesthetics*/}
-            {/*        }}*/}
-            {/*    ></iframe>*/}
-            {/*</div>*/}
-
-            {/*<video*/}
-            {/*    width="100%"*/}
-            {/*    controls={true}*/}
-            {/*    poster="https://via.placeholder.com/640x360.png" // Отображается до начала воспроизведения*/}
-            {/*    style={{borderRadius: '10px'}} // Пример кастомизации стилей*/}
-            {/*>*/}
-            {/*    <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4"/>*/}
-            {/*    Ваш браузер не поддерживает видео. yf*/}
-            {/*</video>*/}
-
-
-            {/*<INITDivider color='transparent' thickness="10%"/>*/}
-
-            {/*<Section*/}
-            {/*    style={roundedCellStyle}*/}
-            {/*    header="Login"*/}
-            {/*>*/}
-            {/*    <Input header="Name" placeholder="Введите имя"/>*/}
-            {/*    <Input header="Email" placeholder="Введите email"/>*/}
-            {/*    <Input header="Password" placeholder="Введите пароль"/>*/}
-            {/*    /!*<Input status="error" header="Input" placeholder="I am error input, don't make my mistakes..." />*!/*/}
-            {/*    /!*<Input status="focused" header="Input" placeholder="I am focused input, are u focused on me?" />*!/*/}
-            {/*    /!*<Input disabled header="Input" placeholder="I am disabled input" />*!/*/}
-
-            {/*</Section>*/}
+            <span>{tg.initDataUnsafe}</span>
         </AppRoot>
     );
 };
