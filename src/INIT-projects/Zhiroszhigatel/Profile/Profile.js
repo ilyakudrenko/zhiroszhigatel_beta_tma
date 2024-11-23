add24eca595a11214344c74010a15d40d19400a9
ilyakudrenko <ilya.kudrenko@gmail.com>
2024-11-22 20:48:40 -0600

    testing WebApp

--- src/INIT-projects/Zhiroszhigatel/Profile/Profile.js
+++ src/INIT-projects/Zhiroszhigatel/Profile/Profile.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {AppRoot, Button, Input, Section} from "@telegram-apps/telegram-ui";
+import React, {useEffect, useState} from 'react';
+import {AppRoot, Button, Input, Section, Text} from "@telegram-apps/telegram-ui";
 import {useNavigate} from "react-router-dom";
 import INITDivider from "../../CustomComponents/Dividers/Divider";
 // import tg from "https://telegram.org/js/telegram-web-app.js?56";
@@ -8,7 +8,6 @@ import INITDivider from "../../CustomComponents/Dividers/Divider";
 
 const Profile = () => {
     const navigate = useNavigate();
-    const tg = window.Telegram.WebApp;
 
     return (
         <AppRoot>
@@ -22,7 +21,11 @@ const Profile = () => {
                     Назад
                 </Button>
             </div>
-            <span>{tg.initDataUnsafe.user.id}</span>
+            <div>
+                <Text>
+                    {window.Telegram.WebApp.initDataUnsafe?.user?.username}
+                </Text>
+            </div>
         </AppRoot>
     );
 };
