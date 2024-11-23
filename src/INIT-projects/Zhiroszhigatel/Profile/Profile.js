bf6b95f399d74b7fec15bd6188559c8536084b0c
ilyakudrenko <ilya.kudrenko@gmail.com>
2024-11-22 20:54:28 -0600

    testing profile getting user data

--- src/INIT-projects/Zhiroszhigatel/Profile/Profile.js
+++ src/INIT-projects/Zhiroszhigatel/Profile/Profile.js
@@ -1,32 +1,37 @@
 import React, {useEffect, useState} from 'react';
-import {AppRoot, Button, Input, Section, Text} from "@telegram-apps/telegram-ui";
+// import {AppRoot, Button, Input, Section, Text} from "@telegram-apps/telegram-ui";
 import {useNavigate} from "react-router-dom";
-import INITDivider from "../../CustomComponents/Dividers/Divider";
+// import INITDivider from "../../CustomComponents/Dividers/Divider";
 // import tg from "https://telegram.org/js/telegram-web-app.js?56";
 
 
 
 const Profile = () => {
     const navigate = useNavigate();
-
+    const tg = window.Telegram.WebApp;
     return (
-        <AppRoot>
-
-            <div>
-                <Button
-                    mode="plain"
-                    size="s"
-                    onClick={() => navigate("/")}
-                >
-                    Назад
-                </Button>
-            </div>
-            <div>
-                <Text>
-                    {window.Telegram.WebApp.initDataUnsafe?.user?.username}
-                </Text>
-            </div>
-        </AppRoot>
+        <div>
+            <span>
+                {tg.initDataUnsafe?.user?.first_name}
+            </span>
+        </div>
+        // <AppRoot>
+        //
+        //     <div>
+        //         <Button
+        //             mode="plain"
+        //             size="s"
+        //             onClick={() => navigate("/")}
+        //         >
+        //             Назад
+        //         </Button>
+        //     </div>
+        //     {/*<div>*/}
+        //     {/*    <Text>*/}
+        //     {/*        {window.Telegram.WebApp.initDataUnsafe?.user?.username}*/}
+        //     {/*    </Text>*/}
+        //     {/*</div>*/}
+        // </AppRoot>
     );
 };
 
