4715ec38ce9124bb72a90bfa67098031d40082b8
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2025-01-03 01:59:10 -0800

    Fixed the link in the GuideButton.js on the get request

--- src/INIT-projects/Zhiroszhigatel/CustomGuides/GuideButton.js
+++ src/INIT-projects/Zhiroszhigatel/CustomGuides/GuideButton.js
@@ -18,7 +18,7 @@ const GuideButton = ({ guide_id, title }) => {
 
                 // Check if the guide is already in the user's library
                 const response = await axios.get(
-                    `https://init-railway-backend-production.up.railway.app/user_guides/check/${userId}/${guide_id}`
+                    `https://init-railway-backend-production.up.railway.app/user_guides/${userId}/${guide_id}`
                 );
 
                 setIsAdded(response.data.exists); // Update the state based on the response
