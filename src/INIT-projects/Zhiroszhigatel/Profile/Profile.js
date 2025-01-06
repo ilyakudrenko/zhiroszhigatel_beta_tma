6346351b6bb51af9a9666cd5980398e31d6a9de6
ilyakudrenko <ilya.kudrenko@gmail.com>
2025-01-05 23:03:53 -0800

    testing calculator page

--- src/INIT-projects/Zhiroszhigatel/Profile/Profile.js
+++ src/INIT-projects/Zhiroszhigatel/Profile/Profile.js
@@ -86,6 +86,40 @@ const Profile = () => {
 
     return (
         <AppRoot>
+            {/*<div>*/}
+            {/*    <h1>Welcome to your Profile!</h1> // updated profile frontend goes here*/}
+            {/*    {userSession && (*/}
+            {/*        <>*/}
+            {/*            <p><strong>ID (DB):</strong> {userSession.id_db}</p>*/}
+            {/*            <p><strong>Username (DB):</strong> {userSession.username_db}</p>*/}
+            {/*            <p><strong>Registration Date (DB):</strong> {userSession.registration_date_db}</p>*/}
+            {/*            <p><strong>Last Login (DB):</strong> {userSession.last_login_db}</p>*/}
+            {/*            <p><strong>Photo URL (TG):</strong> {userSession.photo_url_tg} </p>*/}
+            {/*            <p><strong>First Name (TG):</strong> {userSession.first_name_tg}</p>*/}
+            {/*            <p><strong>Last Name (TG):</strong> {userSession.last_name_tg}</p>*/}
+            {/*            <p><strong>Is Bot (TG):</strong> {userSession.is_bot_tg ? "Yes" : "No"}</p>*/}
+            {/*            <p><strong>Session Started At:</strong> {userSession.time_started_db}</p>*/}
+            {/*        </>*/}
+            {/*    )}*/}
+            {/*</div>*/}
+            <Section>
+                <Cell
+                    //after={<Badge type="number">99</Badge>}
+                    before={
+                        <Avatar
+                            size={48}
+                            src={userSession.photo_url_tg}
+                        />
+                    }
+                    //description="Description"
+                    //subhead={userSession.id_db}
+                    subtitle={userSession.username_db}
+                    //titleBadge={<Badge type="dot" />}
+                >
+                    {userSession.first_name_tg} {userSession.last_name_tg}
+                </Cell>
+            </Section>
+            <INITDivider color='transparent' thickness="10%"/>
             <Section header="Калькулятор калорий">
                 <List
                     style={{
@@ -143,40 +177,6 @@ const Profile = () => {
                     Рассчитать
                 </Button>
             </Section>
-            {/*<div>*/}
-            {/*    <h1>Welcome to your Profile!</h1> // updated profile frontend goes here*/}
-            {/*    {userSession && (*/}
-            {/*        <>*/}
-            {/*            <p><strong>ID (DB):</strong> {userSession.id_db}</p>*/}
-            {/*            <p><strong>Username (DB):</strong> {userSession.username_db}</p>*/}
-            {/*            <p><strong>Registration Date (DB):</strong> {userSession.registration_date_db}</p>*/}
-            {/*            <p><strong>Last Login (DB):</strong> {userSession.last_login_db}</p>*/}
-            {/*            <p><strong>Photo URL (TG):</strong> {userSession.photo_url_tg} </p>*/}
-            {/*            <p><strong>First Name (TG):</strong> {userSession.first_name_tg}</p>*/}
-            {/*            <p><strong>Last Name (TG):</strong> {userSession.last_name_tg}</p>*/}
-            {/*            <p><strong>Is Bot (TG):</strong> {userSession.is_bot_tg ? "Yes" : "No"}</p>*/}
-            {/*            <p><strong>Session Started At:</strong> {userSession.time_started_db}</p>*/}
-            {/*        </>*/}
-            {/*    )}*/}
-            {/*</div>*/}
-            <Section>
-                <Cell
-                    //after={<Badge type="number">99</Badge>}
-                    before={
-                        <Avatar
-                            size={48}
-                            src={userSession.photo_url_tg}
-                        />
-                    }
-                    //description="Description"
-                    //subhead={userSession.id_db}
-                    subtitle={userSession.username_db}
-                    //titleBadge={<Badge type="dot" />}
-                >
-                    {userSession.first_name_tg} {userSession.last_name_tg}
-                </Cell>
-            </Section>
-            <INITDivider color='transparent' thickness="10%"/>
             <Section>
                 <Cell
                     before={<Icon32ProfileColoredSquare />}
