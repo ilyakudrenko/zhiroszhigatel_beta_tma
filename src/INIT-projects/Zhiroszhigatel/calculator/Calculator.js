2ee5be32732d4d2a316f4afe22de5426697ccfe8
ilyakudrenko <ilya.kudrenko@gmail.com>
2025-01-05 22:54:55 -0800

    testing calculator page

--- src/INIT-projects/Zhiroszhigatel/calculator/Calculator.js
+++ src/INIT-projects/Zhiroszhigatel/calculator/Calculator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     AppRoot,
     Button,
@@ -6,64 +6,82 @@ import {
     IconContainer,
     Input,
     Section,
-    Select
+    Select,
+    List,
+    Tappable
 } from "@telegram-apps/telegram-ui";
 import INITBackButton from "../../../Hooks/BackButton";
-import {Icon28Stats} from "@telegram-apps/telegram-ui/dist/icons/28/stats";
+import { Icon28Stats } from "@telegram-apps/telegram-ui/dist/icons/28/stats";
+import { Icon24Close } from "@telegram-apps/telegram-ui/dist/icons/24/close";
 import INITDivider from "../../CustomComponents/Dividers/Divider";
-
-
 
 const Calculator = () => {
     INITBackButton();
+
+    const [value, setValue] = useState('');
+    const clearInput = () => setValue('');
+
     return (
         <AppRoot>
-            <Section
-                header="Калькулятор калорий"
-                style={{
-                    padding: '16px',
-                }}
-            >
-                <Input
-                    header="Возраст"
-                    placeholder="Укажите ваш возраст"
-                />
-                <Input
-                    header="Рост"
-                    placeholder="Укажите ваш рост в см"
-                />
-                <Input
-                    header="Вес"
-                    placeholder="Укажите ваш вес в кг"
-                />
-                <Select header="Пол" placeholder="I am usual input, just leave me alone">
-                    <option>Мужчина</option>
-                    <option>Женщина</option>
-                </Select>
-                <Select header="Уровень активности" placeholder="I am usual input, just leave me alone">
-                    <option>Минмальная активность</option>
-                    <option>Слабый уровень активности</option>
-                    <option>Умеренный активности</option>
-                    <option>Тяжелая или трудоемкая активность</option>
-                    <option>Экстремальный уровень активности</option>
-                </Select>
+            <Section header="Калькулятор калорий">
+                <List
+                    style={{
+                        width: 400,
+                        maxWidth: '100%',
+                        margin: 'auto',
+                        background: 'var(--tgui--secondary_bg_color)'
+                    }}
+                >
+                    <Input
+                        header="Обычный ввод"
+                        placeholder="Введите значение"
+                    />
+                    <Input
+                        status="error"
+                        header="Ошибка"
+                        placeholder="Ошибка в значении"
+                    />
+                    <Input
+                        status="focused"
+                        header="Фокус"
+                        placeholder="Сфокусированное поле ввода"
+                    />
+                    <Input
+                        disabled
+                        header="Отключено"
+                        placeholder="Поле отключено"
+                    />
+                    <Input
+                        status="focused"
+                        header="Очистить меня"
+                        placeholder="Напишите и очистите"
+                        value={value}
+                        onChange={(e) => setValue(e.target.value)}
+                        after={
+                            <Tappable
+                                Component="div"
+                                style={{ display: 'flex' }}
+                                onClick={clearInput}
+                            >
+                                <Icon24Close />
+                            </Tappable>
+                        }
+                    />
+                </List>
                 <Button
                     mode="filled"
                     size="m"
                     stretched
                     style={{
                         display: 'block', // Makes the button behave like a block-level element
-                        margin: '0 auto', // Centers the block element horizontally.
+                        margin: '20px auto', // Centers the block element horizontally.
                     }}
                 >
                     Рассчитать
                 </Button>
             </Section>
-            <INITDivider color='transparent' thickness="10%"/>
-            <Section
-                footer="доп информация"
-                header="Ваш калораж"
-            >
+            <INITDivider color="transparent" thickness="10%" />
+            <Section footer="доп информация" header="Ваш калораж">
                 <Cell before={<IconContainer><Icon28Stats /></IconContainer>}>
                     Chat Settings
                 </Cell>
