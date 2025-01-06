4570e335fba71aa748c2d811658cebc82dc83275
ilyakudrenko <ilya.kudrenko@gmail.com>
2025-01-05 22:37:47 -0800

    testing calculator page

--- src/INIT-projects/Zhiroszhigatel/calculator/Calculator.js
+++ src/INIT-projects/Zhiroszhigatel/calculator/Calculator.js
@@ -4,7 +4,7 @@ import {
     Button,
     Cell,
     IconContainer,
-    Input,
+    Input, List,
     Section,
     Select
 } from "@telegram-apps/telegram-ui";
@@ -18,59 +18,68 @@ const Calculator = () => {
     INITBackButton();
     return (
         <AppRoot>
-            <Section
-                header="Калькулятор калорий"
+            <List
+                style={{
+                    background: 'var(--tgui--secondary_bg_color)',
+                    padding: '40px',
+                    width: 500
+                }}
             >
-                <Input
-                    header="Возраст"
-                    placeholder="Укажите ваш возраст"
-                />
-                <Input
-                    header="Рост"
-                    placeholder="Укажите ваш рост в см"
-                />
-                <Input
-                    header="Вес"
-                    placeholder="Укажите ваш вес в кг"
-                />
-                <Select header="Пол" placeholder="I am usual input, just leave me alone">
-                    <option>Мужчина</option>
-                    <option>Женщина</option>
-                </Select>
-                <Select header="Уровень активности" placeholder="I am usual input, just leave me alone">
-                    <option>Минмальная активность</option>
-                    <option>Слабый уровень активности</option>
-                    <option>Умеренный активности</option>
-                    <option>Тяжелая или трудоемкая активность</option>
-                    <option>Экстремальный уровень активности</option>
-                </Select>
-                <Button
-                    mode="filled"
-                    size="m"
-                    stretched
-                    style={{
-                        display: 'block', // Makes the button behave like a block-level element
-                        margin: '0 auto', // Centers the block element horizontally.
-                    }}
+                <Section
+                    header="Калькулятор калорий"
                 >
-                    Рассчитать
-                </Button>
-            </Section>
-            <INITDivider color='transparent' thickness="10%"/>
-            <Section
-                footer="доп информация"
-                header="Ваш калораж"
-            >
-                <Cell before={<IconContainer><Icon28Stats /></IconContainer>}>
-                    Chat Settings
-                </Cell>
-                <Cell before={<IconContainer><Icon28Stats /></IconContainer>}>
-                    Data and Storage
-                </Cell>
-                <Cell before={<IconContainer><Icon28Stats /></IconContainer>}>
-                    Devices
-                </Cell>
-            </Section>
+                    <Input
+                        header="Возраст"
+                        placeholder="Укажите ваш возраст"
+                    />
+                    <Input
+                        header="Рост"
+                        placeholder="Укажите ваш рост в см"
+                    />
+                    <Input
+                        header="Вес"
+                        placeholder="Укажите ваш вес в кг"
+                    />
+                    <Select header="Пол" placeholder="I am usual input, just leave me alone">
+                        <option>Мужчина</option>
+                        <option>Женщина</option>
+                    </Select>
+                    <Select header="Уровень активности" placeholder="I am usual input, just leave me alone">
+                        <option>Минмальная активность</option>
+                        <option>Слабый уровень активности</option>
+                        <option>Умеренный активности</option>
+                        <option>Тяжелая или трудоемкая активность</option>
+                        <option>Экстремальный уровень активности</option>
+                    </Select>
+                    <Button
+                        mode="filled"
+                        size="m"
+                        stretched
+                        style={{
+                            display: 'block', // Makes the button behave like a block-level element
+                            margin: '0 auto', // Centers the block element horizontally.
+                        }}
+                    >
+                        Рассчитать
+                    </Button>
+                </Section>
+                <INITDivider color='transparent' thickness="10%"/>
+                <Section
+                    footer="доп информация"
+                    header="Ваш калораж"
+                >
+                    <Cell before={<IconContainer><Icon28Stats /></IconContainer>}>
+                        Chat Settings
+                    </Cell>
+                    <Cell before={<IconContainer><Icon28Stats /></IconContainer>}>
+                        Data and Storage
+                    </Cell>
+                    <Cell before={<IconContainer><Icon28Stats /></IconContainer>}>
+                        Devices
+                    </Cell>
+                </Section>
+            </List>
+
         </AppRoot>
     );
 };
