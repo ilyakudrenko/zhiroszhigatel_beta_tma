28b04895824264f3d1492e8adbae3c01cdf0505f
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2025-01-08 02:39:05 -0800

    updated test connection

--- src/INIT-projects/Zhiroszhigatel/TestPages/testPage.js
+++ src/INIT-projects/Zhiroszhigatel/TestPages/testPage.js
@@ -5,20 +5,35 @@ const TestConnection = () => {
     const [status, setStatus] = useState(null);
     const [error, setError] = useState(null);
     const testConnection = async () => {
+        try{
             axios.get('https://init-railway-backend-v2-production.up.railway.app/test-db', {
                 withCredentials: true
             })
-                .then(response => console.log(response.data))
-                .catch(error => console.error("Fetch error:", error));
+                setStatus(response.data.message);
+                setError(null);
+                // .then(response => console.log(response.data))
+                // .catch(error => console.error("Fetch error:", error));
+        }catch(error){
+            setError(error.message);
+            setStatus(null);
+        }
+
 
     };
 
     return (
-        <div style={{ padding: "20px", textAlign: "center" }}>
+        <div style={{padding: "20px", textAlign: "center"}}>
             <h1>Database Connection Test V2</h1>
-            <button onClick={testConnection} style={{ padding: "10px 20px", cursor: "pointer" }}>
+            <button
+                onClick={testConnection}
+                style={{padding: "10px 20px", cursor: "pointer"}}
+            >
                 Test Connection
             </button>
+            {/* Сообщение об успешной связи */}
+            {status && <p style={{color: "green"}}>{status}</p>}
+            {/* Сообщение об ошибке */}
+            {error && <p style={{color: "red"}}>{error}</p>}
         </div>
     );
 };
