46e96c0a62fa938a7657a557228af5246d14d68e
ilyakudrenko <ilya.kudrenko@gmail.com>
2025-01-07 17:10:00 -0800

    testing connection to backend

--- src/INIT-projects/Zhiroszhigatel/TestPages/testPage.js
+++ src/INIT-projects/Zhiroszhigatel/TestPages/testPage.js
@@ -9,12 +9,9 @@ const TestPage = () => {
 
     const testConnection = async () => {
         try {
-            console.log("Sending request to backend...");
-            const response = await axios.get(
-                "https://init-railway-backend-production.up.railway.app/connection_test/test",
-                { withCredentials: true } // Ensures proper CORS handling
-            );
-            console.log("Response received:", response);
+            const response = await axios.get("https://init-railway-backend-production.up.railway.app/users/test");
+            setConnectionStatus(response.data.message); // Отображает сообщение успешного подключения
+            setError(null);
         } catch (err) {
             setError(err.message); // Отображает ошибку, если подключение не удалось
             setConnectionStatus(null);
