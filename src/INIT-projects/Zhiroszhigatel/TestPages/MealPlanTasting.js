8dc20a0ba2f349a5384293fdbc19366858000da5
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2025-01-11 01:33:59 -0800

    testing mealPlans test page

--- src/INIT-projects/Zhiroszhigatel/TestPages/MealPlanTasting.js
+++ src/INIT-projects/Zhiroszhigatel/TestPages/MealPlanTasting.js
@@ -23,29 +23,7 @@ const MealPlanTasting = () => {
         <div style={{ padding: "20px", textAlign: "center" }}>
             <h1>User Meal Plans</h1>
             {error && <p style={{ color: "red" }}>{error}</p>}
-            {mealPlans.length > 0 ? (
-                <ul style={{ listStyleType: "none", padding: 0 }}>
-                    {mealPlans.map((mealPlan) => (
-                        <li
-                            key={mealPlan.mealPlan_id}
-                            style={{
-                                marginBottom: "15px",
-                                border: "1px solid #ccc",
-                                padding: "10px",
-                                borderRadius: "5px",
-                                textAlign: "left",
-                                maxWidth: "400px",
-                                margin: "0 auto 15px",
-                            }}
-                        >
-                            <h3>{mealPlan.mealPlan_title}</h3>
-                            <p>{mealPlan.mealPlan_description || "No description available."}</p>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>Loading meal plans...</p>
-            )}
+            {mealPlans}
         </div>
     );
 };
