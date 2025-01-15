87e2a57a7e46b317f1a7ad90166188a13c2e65e0
ilyakudrenko <ilya.kudrenko@gmail.com>
2025-01-15 13:33:51 -0800

    testing middle page for mealplans

--- src/INIT-projects/Zhiroszhigatel/TestPages/MealPlanTasting.js
+++ src/INIT-projects/Zhiroszhigatel/TestPages/MealPlanTasting.js
@@ -68,32 +68,36 @@ const MealPlanTasting = () => {
 
             <INITDivider color='transparent' thickness="10%"/>
 
-            <Title level="2" weight="bold" style={{marginBottom: "10px"}}>
-                {mealPlans[0]?.mealPlan_title || "Meal Plan"}
-            </Title>
-            {mealPlanDays.map((day, index) => (
-                <Section key={index} header={`Day ${day.mealPlanDays_day_number}`}>
-                    <Cell>
-                        <b>Calories:</b> {day.mealPlanDays_total_kcal} kcal
-                    </Cell>
-                    <Cell>
-                        <b>Proteins:</b> {day.mealPlanDays_total_protein} g
-                    </Cell>
-                    <Cell>
-                        <b>Fats:</b> {day.mealPlanDays_total_fat} g
-                    </Cell>
-                    <Cell>
-                        <b>Carbs:</b> {day.mealPlanDays_total_carbs} g
-                    </Cell>
-                    <INITDivider color='light' thickness="1px"/>
-                    <Caption level="1" weight="3" style={{marginBottom: "10px"}}>
-                        Plan Details
-                    </Caption>
-                    <Cell multiline>
-                        {mealPlans[0]?.mealPlan_description || "No additional details available."}
-                    </Cell>
-                </Section>
-            ))}
+            <List>
+                <Title level="2" weight="bold" style={{marginBottom: "10px"}}>
+                    {mealPlans[0]?.mealPlan_title || "Meal Plan"}
+                </Title>
+                <Caption
+                    caps
+                    level="1"
+                    weight="3"
+                    style={{margin: '5%'}}
+                    multiline
+                >
+                    {mealPlans[0]?.mealPlan_description || "No additional details available."}
+                </Caption>
+                {mealPlanDays.map((day, index) => (
+                    <Section key={index} header={`Day ${day.mealPlanDays_day_number}`}>
+                        <Cell>
+                            <b>Calories:</b> {day.mealPlanDays_total_kcal} kcal
+                        </Cell>
+                        <Cell>
+                            <b>Proteins:</b> {day.mealPlanDays_total_protein} g
+                        </Cell>
+                        <Cell>
+                            <b>Fats:</b> {day.mealPlanDays_total_fat} g
+                        </Cell>
+                        <Cell>
+                            <b>Carbs:</b> {day.mealPlanDays_total_carbs} g
+                        </Cell>
+                    </Section>
+                ))}
+            </List>
         </AppRoot>
 
         // <AppRoot>
