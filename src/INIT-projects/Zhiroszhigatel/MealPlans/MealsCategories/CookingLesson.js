9a745df26a459592d5a93c9e4d34f7b203e84428
ilyakudrenko <ilya.kudrenko@gmail.com>
2025-01-15 18:52:38 -0800

    adjusting content sections

--- src/INIT-projects/Zhiroszhigatel/MealPlans/MealsCategories/CookingLesson.js
+++ src/INIT-projects/Zhiroszhigatel/MealPlans/MealsCategories/CookingLesson.js
@@ -10,72 +10,81 @@ const CookingLesson = () => {
 
     return (
         <AppRoot>
+            <List>
+                <Section>
+                    <Cell
+                        multiline
+                        subhead="5 Рецептов"
+                    >
+                        <div style={{textAlign: "center"}}>
+                            <iframe
+                                src="https://vkvideo.ru/video_ext.php?oid=9490543&id=456239820&hash=432b6138a9f4c282"
+                                width="90%" height="215" frameBorder="0" allowFullScreen="1"
+                                allow="autoplay; encrypted-media; fullscreen; picture-in-picture"
+                                style={{
+                                    border: "none", // Removes the border
+                                    borderRadius: 16,
+                                    padding: 5,
+                                }}
+                            ></iframe>
+                        </div>
+                    </Cell>
+                    <Cell
+                        multiline
+                        subhead="Готовый_2"
+                    >
+                        <div style={{textAlign: "center"}}>
+                            <iframe
+                                src="https://vkvideo.ru/video_ext.php?oid=9490543&id=456239822&hash=ecc17305affbf253"
+                                width="90%" height="215" frameBorder="0" allowFullScreen="1"
+                                allow="autoplay; encrypted-media; fullscreen; picture-in-picture"
+                                style={{
+                                    border: "none", // Removes the border
+                                    borderRadius: 16,
+                                    padding: 5,
+                                }}
+                            ></iframe>
+                        </div>
+                    </Cell>
+                    <Cell
+                        multiline
+                        subhead="Кулинарные утехи на диете"
+                    >
+                        <div style={{textAlign: "center"}}>
+                            <iframe
+                                width="90%"
+                                height="215"
+                                src="https://www.youtube.com/embed/_wUqQAZOjuk"
+                                style={{
+                                    border: "none", // Removes the border
+                                    borderRadius: 16,
+                                    padding: 5,
+                                }}
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            ></iframe>
+                        </div>
+                    </Cell>
+                    <Cell
+                        multiline
+                        subhead="Урок с кухни, готовим вкусняхи"
+                    >
+                        <div style={{textAlign: "center"}}>
+                            <iframe
+                                src="https://vkvideo.ru/video_ext.php?oid=9490543&id=456239827&hash=26d79dcb8b19ca66"
+                                width="90%" height="215" frameBorder="0" allowFullScreen="1"
+                                allow="autoplay; encrypted-media; fullscreen; picture-in-picture"
+                                style={{
+                                    border: "none", // Removes the border
+                                    borderRadius: 16,
+                                    padding: 5,
+                                }}
+                            ></iframe>
+                        </div>
+                    </Cell>
+                </Section>
+            </List>
 
-            {/*5 Рецептов*/}
-            <Section header="5 Рецептов">
-                <div style={{textAlign: "center"}}>
-                    <iframe src="https://vkvideo.ru/video_ext.php?oid=9490543&id=456239820&hash=432b6138a9f4c282"
-                            width="90%" height="215" frameBorder="0" allowFullScreen="1"
-                            allow="autoplay; encrypted-media; fullscreen; picture-in-picture"
-                            style={{
-                                border: "none", // Removes the border
-                                borderRadius: 16,
-                                padding: 5,
-                            }}
-                    ></iframe>
-                </div>
-            </Section>
-            <INITDivider color='transparent' thickness="10%"/>
-
-            {/*Готовый_2*/}
-            <Section header="Готовый_2">
-                <div style={{textAlign: "center"}}>
-                    <iframe src="https://vkvideo.ru/video_ext.php?oid=9490543&id=456239822&hash=ecc17305affbf253"
-                            width="90%" height="215" frameBorder="0" allowFullScreen="1"
-                            allow="autoplay; encrypted-media; fullscreen; picture-in-picture"
-                            style={{
-                                border: "none", // Removes the border
-                                borderRadius: 16,
-                                padding: 5,
-                            }}
-                    ></iframe>
-                </div>
-            </Section>
-            <INITDivider color='transparent' thickness="10%"/>
-
-            {/*Кулинарные утехи на диете*/}
-            <Section header="Кулинарные утехи на диете">
-                <div style={{textAlign: "center"}}>
-                    <iframe
-                        width="90%"
-                        height="215"
-                        src="https://www.youtube.com/embed/_wUqQAZOjuk"
-                        style={{
-                            border: "none", // Removes the border
-                            borderRadius: 16,
-                            padding: 5,
-                        }}
-                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                    ></iframe>
-                </div>
-            </Section>
-            <INITDivider color='transparent' thickness="10%"/>
-
-            {/*Урок с кухни, готовим вкусняхи*/}
-            <Section header="Урок с кухни, готовим вкусняхи">
-                <div style={{textAlign: "center"}}>
-                    <iframe src="https://vkvideo.ru/video_ext.php?oid=9490543&id=456239827&hash=26d79dcb8b19ca66"
-                            width="90%" height="215" frameBorder="0" allowFullScreen="1"
-                            allow="autoplay; encrypted-media; fullscreen; picture-in-picture"
-                            style={{
-                                border: "none", // Removes the border
-                                borderRadius: 16,
-                                padding: 5,
-                            }}
-                    ></iframe>
-                </div>
-            </Section>
             <INITDivider color='transparent' thickness="15%"/>
 
             <List>
@@ -84,131 +93,172 @@ const CookingLesson = () => {
                 </Title>
                 <INITDivider color='transparent' thickness="10%"/>
                 <Section>
-                    <Cell
-                        multiline
-                        subhead="ЧебуПицца"
-                    >
+                    <Cell multiline subhead="ЧебуПицца">
                         <ul>
                             <li>фарш куриный 170гр</li>
                             <li>Лук ¼, зелень для фарша</li>
                             <li>яйцо куриное – 1шт</li>
                             <li>Помидор – ½ шт</li>
-                            <li>Помидор – ½ шт</li>
-                            <li>Помидор – ½ шт</li>
                             <li>Сыр легкий – 60гр</li>
                         </ul>
                     </Cell>
-                    <Cell
-                        multiline
-                        subhead="Ленивые вареники"
-                    >
-                        - творог 200гр
-                        - яйцо 1шт
-                        - мука рисовая 60гр
-                        - подсластитель стевия
-                        - варим в кипящей подсоленной воде 1 мин после всплытия
+                    <Cell multiline subhead="Ленивые вареники">
+                        <ul>
+                            <li>творог 200гр</li>
+                            <li>яйцо 1шт</li>
+                            <li>мука рисовая 60гр</li>
+                            <li>подсластитель стевия</li>
+                            <li>варим в кипящей подсоленной воде 1 мин после всплытия</li>
+                        </ul>
                     </Cell>
-                    <Cell
-                        multiline
-                        subhead="Протеиновое печенье"
-                    >
-                        - 200гр овсянки
-                        - 70 гр протеина
-                        - яйца 3шт
-                        - молоко 60мл
-                        Творог (пожеланию)
-                        Какао (считать) , корица по желанию
-                        20-25 мин в духовке
+                    <Cell multiline subhead="Протеиновое печенье">
+                        <ul>
+                            <li>200гр овсянки</li>
+                            <li>70 гр протеина</li>
+                            <li>яйца 3шт</li>
+                            <li>молоко 60мл</li>
+                            <li>Творог (по желанию)</li>
+                            <li>Какао (считать), корица по желанию</li>
+                            <li>20-25 мин в духовке</li>
+                        </ul>
                     </Cell>
-                    <Cell
-                        multiline
-                        subhead="Куриные тефтели с рисом"
-                    >
-                        - куриный фарш из филе грудки -450гр
-                        - рис белый/бурый
-                        - специи, соль чеснок, зелень
-                        - морковь 1 шт на терке на дно формы
-                        Для заливки Вода + йогурт +томатная паста
-                        40 мин в духовке
+                    <Cell multiline subhead="Куриные тефтели с рисом">
+                        <ul>
+                            <li>куриный фарш из филе грудки -450гр</li>
+                            <li>рис белый/бурый</li>
+                            <li>специи, соль чеснок, зелень</li>
+                            <li>морковь 1 шт на терке на дно формы</li>
+                            <li>Для заливки: Вода + йогурт + томатная паста</li>
+                            <li>40 мин в духовке</li>
+                        </ul>
                     </Cell>
-                    <Cell
-                        multiline
-                        subhead="Салат с тунцом"
-                    >
-                        - нарвать лист салата
-                        -Огурец 1шт
-                        - Яйцо вареное 2шт
-                        - Болгарский перец ½ шт
-                        - Тунец консервированный 150-200 гр
-                        Сыр легкий на терке 10-20гр
+                    <Cell multiline subhead="Салат с тунцом">
+                        <ul>
+                            <li>нарвать лист салата</li>
+                            <li>Огурец 1шт</li>
+                            <li>Яйцо вареное 2шт</li>
+                            <li>Болгарский перец ½ шт</li>
+                            <li>Тунец консервированный 150-200 гр</li>
+                            <li>Сыр легкий на терке 10-20гр</li>
+                        </ul>
                     </Cell>
-                    <Cell
-                        multiline
-                        subhead="Рол с тунцом (2шт)"
-                    >
-                        Тортилья – 2шт
-                        - Тунец или другая консерва рыбная (но не масле) – 80-100гр
-                        - Яйцо 1шт
-                        - Помидор ¼ шт
-                        - Лук зеленый
-                        - 1ст ложка йогурта
-                        - лист салата
+                    <Cell multiline subhead="Рол с тунцом (2шт)">
+                        <ul>
+                            <li>Тортилья – 2шт</li>
+                            <li>Тунец или другая консерва рыбная (но не в масле) – 80-100гр</li>
+                            <li>Яйцо 1шт</li>
+                            <li>Помидор ¼ шт</li>
+                            <li>Лук зеленый</li>
+                            <li>1 ст ложка йогурта</li>
+                            <li>лист салата</li>
+                        </ul>
                     </Cell>
-                    <Cell
-                        multiline
-                        subhead="Рол с курицей (2шт)"
-                    >
-                        -Тортилья 2 шт
-                        - Копченая грудка 100гр
-                        - Горчица + ст ложка йогрута
-                        - Яйцо вареное 1шт
-                        - помидор 1/4 шт
-                        - огурец, квашеная капуста по желанию
-                        - Лист салата
+                    <Cell multiline subhead="Рол с курицей (2шт)">
+                        <ul>
+                            <li>Тортилья 2 шт</li>
+                            <li>Копченая грудка 100гр</li>
+                            <li>Горчица + 1 ст ложка йогурта</li>
+                            <li>Яйцо вареное 1шт</li>
+                            <li>Помидор 1/4 шт</li>
+                            <li>Огурец, квашеная капуста по желанию</li>
+                            <li>Лист салата</li>
+                        </ul>
                     </Cell>
-                    <Cell
-                        multiline
-                        subhead="Протеиновое желе"
-                    >
-                        Можно с протеином, можно на йогурте, на воде, молоке, как захотите
-                        Понадобиться желатин быстрорастворимый 10-20 гр в зависимости сколько желе будете делать
+                    <Cell multiline subhead="Протеиновое желе">
+                        <ul>
+                            <li>Можно с протеином, можно на йогурте, на воде, молоке, как захотите</li>
+                            <li>Понадобится желатин быстрорастворимый 10-20 гр</li>
+                        </ul>
                     </Cell>
-                    <Cell
-                        multiline
-                        subhead="Куриная колбаса"
-                    >
-                        300 гр фарша из филе грудки, 300 гр резаного мяса бедра курицы
-                        Соль, перец, чеснок
-                        Ледяная вода или молоко 60мл
-                        В морозилку на 1 час
-                        Заворачиваем колбасу в рукав для запекания
-                        Варить 1 час при температуре 80 гр (то есть чтоб вода не кипела)
-                        После варки охладить водой потом в холодильник на часов 5-8
+                    <Cell multiline subhead="Куриная колбаса">
+                        <ul>
+                            <li>300 гр фарша из филе грудки</li>
+                            <li>300 гр резаного мяса бедра курицы</li>
+                            <li>Соль, перец, чеснок</li>
+                            <li>Ледяная вода или молоко 60мл</li>
+                            <li>В морозилку на 1 час</li>
+                            <li>Заворачиваем колбасу в рукав для запекания</li>
+                            <li>Варить 1 час при температуре 80 гр (чтобы вода не кипела)</li>
+                            <li>После варки охладить водой, потом в холодильник на 5-8 часов</li>
+                        </ul>
                     </Cell>
-                    <Cell
-                        multiline
-                        subhead="Боул Поке"
-                    >
-                        Рис или любая другая крупа
-                        Курица или рыба
-                        Авокадо
-                        Огурец помидор
-                        фасоль
-                        Кунжутом можно посыпать
+                    <Cell multiline subhead="Боул Поке">
+                        <ul>
+                            <li>Рис или любая другая крупа</li>
+                            <li>Курица или рыба</li>
+                            <li>Авокадо</li>
+                            <li>Огурец, помидор</li>
+                            <li>Фасоль</li>
+                            <li>Кунжутом можно посыпать</li>
+                        </ul>
                     </Cell>
-                    <Cell
-                        multiline
-                        subhead="Запеканка творожная"
-                    >
-                        Творог 200гр
-                        Мука любая 60гр
-                        Яйцо 1 шт
-                        Водсластитель
-                        Добавить фрукт по желанию
+                    <Cell multiline subhead="Запеканка творожная">
+                        <ul>
+                            <li>Творог 200гр</li>
+                            <li>Мука любая 60гр</li>
+                            <li>Яйцо 1 шт</li>
+                            <li>Подсластитель</li>
+                            <li>Добавить фрукт по желанию</li>
+                        </ul>
                     </Cell>
                 </Section>
-
             </List>
+
+            <INITDivider color='transparent' thickness="15%"/>
+
+            <List>
+                <Section>
+                    <Cell
+                        multiline
+                        subhead=""
+                    >
+
+                    </Cell>
+                    <Cell
+                        multiline
+                        subhead=""
+                    >
+
+                    </Cell>
+                    <Cell
+                        multiline
+                        subhead=""
+                    >
+
+                    </Cell>
+                    <Cell
+                        multiline
+                        subhead=""
+                    >
+
+                    </Cell>
+                    <Cell
+                        multiline
+                        subhead=""
+                    >
+
+                    </Cell>
+                    <Cell
+                        multiline
+                        subhead=""
+                    >
+
+                    </Cell>
+                    <Cell
+                        multiline
+                        subhead=""
+                    >
+
+                    </Cell>
+                    <Cell
+                        multiline
+                        subhead=""
+                    >
+
+                    </Cell>
+                </Section>
+            </List>
+
         </AppRoot>
     );
 };
