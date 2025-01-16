f6e7ac57efa599aee535f60dd03a3e2b0aada670
ilyakudrenko <ilya.kudrenko@gmail.com>
2025-01-16 01:47:59 -0800

    Bonus modal page is added

--- src/INIT-projects/Zhiroszhigatel/MealPlans/MealsCategories/Bonus.js
+++ src/INIT-projects/Zhiroszhigatel/MealPlans/MealsCategories/Bonus.js
@@ -11,32 +11,22 @@ const INITBonus = () => {
     return (
         <AppRoot>
             <List>
-                <Section multiline>
+                <Section>
                     <Cell multiline>
                         1. Мой лучший друг – желудок. Еда для умных людей. Елена Мотова
                         <INITDivider color='transparent' thickness="10%"/>
                         <img
-                            src="https://marketplace.canva.com/EAFaQMYuZbo/1/0/1003w/canva-brown-rusty-mystery-novel-book-cover-hG1QhA7BiBU.jpg"
+                            src="/BooksIMG/book1.jpeg"
                             style={{width: '100%', display: 'block'}}
                             alt="Book Cover"
                         />
                     </Cell>
-                    {/*<Cell>*/}
-                    {/*    <a*/}
-                    {/*        href="https://fs02.getcourse.ru/fileservice/file/download/a/562611/sc/156/h/d66edc46b3387fca4093854c71732af6.zip"*/}
-                    {/*        target="_blank"*/}
-                    {/*        rel="noopener noreferrer"*/}
-                    {/*        style={{textDecoration: 'none', color: 'inherit'}}*/}
-                    {/*    >*/}
-                    {/*        📥Ссылка на скачивание*/}
-                    {/*    </a>*/}
-                    {/*</Cell>*/}
                     <Cell>
                         <a
                             href="https://fs02.getcourse.ru/fileservice/file/download/a/562611/sc/156/h/d66edc46b3387fca4093854c71732af6.zip"
                             target="_blank"
                             rel="noopener noreferrer"
-                            style={{ textDecoration: "none", color: "inherit" }}
+                            style={{color: "inherit" }}
                         >
                             📥 Скачать ZIP файл
                         </a>
@@ -44,43 +34,95 @@ const INITBonus = () => {
                 </Section>
                 <INITDivider color='transparent' thickness="10%"/>
 
-                <Section multiline>
-                    2. Еда для радости. Записки диетолога. Елена Мотова
-                    <img
-                        src="https://marketplace.canva.com/EAFaQMYuZbo/1/0/1003w/canva-brown-rusty-mystery-novel-book-cover-hG1QhA7BiBU.jpg"
-                        style={{width: '100%', display: 'block'}}
-                        alt="Book Cover"
-                    />
+                <Section>
+                    <Cell multiline>
+                        2. Еда для радости. Записки диетолога. Елена Мотова
+                        <INITDivider color='transparent' thickness="10%"/>
+                        <img
+                            src="/BooksIMG/book2.jpeg"
+                            style={{width: '100%', display: 'block'}}
+                            alt="Book Cover"
+                        />
+                    </Cell>
+                    <Cell>
+                        <a
+                            href="https://fs17.getcourse.ru/fileservice/file/download/a/562611/sc/115/h/38f3c4938ec425298c8245cea19ad484.zip"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={{color: "inherit" }}
+                        >
+                            📥 Скачать ZIP файл
+                        </a>
+                    </Cell>
                 </Section>
                 <INITDivider color='transparent' thickness="10%"/>
 
-                <Section multiline>
-                    3. Очаровательный кишечник. Как самый могущественный орган управляет нами. Джулия Эндерс
-                    <img
-                        src="https://marketplace.canva.com/EAFaQMYuZbo/1/0/1003w/canva-brown-rusty-mystery-novel-book-cover-hG1QhA7BiBU.jpg"
-                        style={{width: '100%', display: 'block'}}
-                        alt="Book Cover"
-                    />
+                <Section>
+                    <Cell multiline>
+                        3. Очаровательный кишечник. Как самый могущественный орган управляет нами. Джулия Эндерс
+                        <INITDivider color='transparent' thickness="10%"/>
+                        <img
+                            src=""
+                            style={{width: '100%', display: 'block'}}
+                            alt="Book Cover"
+                        />
+                    </Cell>
+                    <Cell>
+                        <a
+                            href="https://fs24.getcourse.ru/fileservice/file/download/a/562611/sc/466/h/379b049c08efe5153881690305b102e5.zip"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={{color: "inherit" }}
+                        >
+                            📥 Скачать ZIP файл
+                        </a>
+                    </Cell>
                 </Section>
                 <INITDivider color='transparent' thickness="10%"/>
 
-                <Section multiline>
-                    4. Голодный мозг. Как перехитрить инстинкты, которые заставляют нас переедать
-                    <img
-                        src="https://marketplace.canva.com/EAFaQMYuZbo/1/0/1003w/canva-brown-rusty-mystery-novel-book-cover-hG1QhA7BiBU.jpg"
-                        style={{width: '100%', display: 'block'}}
-                        alt="Book Cover"
-                    />
+                <Section>
+                    <Cell multiline>
+                        4. Голодный мозг. Как перехитрить инстинкты, которые заставляют нас переедать
+                        <INITDivider color='transparent' thickness="10%"/>
+                        <img
+                            src=""
+                            style={{width: '100%', display: 'block'}}
+                            alt="Book Cover"
+                        />
+                    </Cell>
+                    <Cell>
+                        <a
+                            href="https://fs21.getcourse.ru/fileservice/file/download/a/562611/sc/339/h/db4600eda0ad71bf2e98b9d4df59e750.zip"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={{color: "inherit" }}
+                        >
+                            📥 Скачать ZIP файл
+                        </a>
+                    </Cell>
                 </Section>
                 <INITDivider color='transparent' thickness="10%"/>
 
-                <Section multiline>
-                    5. Почему мы толстеем. Мифы и факты о том, что мешает нам быть стройными Гэри Таубс
-                    <img
-                        src="https://marketplace.canva.com/EAFaQMYuZbo/1/0/1003w/canva-brown-rusty-mystery-novel-book-cover-hG1QhA7BiBU.jpg"
-                        style={{width: '100%', display: 'block'}}
-                        alt="Book Cover"
-                    />
+                <Section>
+                    <Cell multiline>
+                        5. Почему мы толстеем. Мифы и факты о том, что мешает нам быть стройными Гэри Таубс
+                        <INITDivider color='transparent' thickness="10%"/>
+                        <img
+                            src=""
+                            style={{width: '100%', display: 'block'}}
+                            alt="Book Cover"
+                        />
+                    </Cell>
+                    <Cell>
+                        <a
+                            href="https://fs19.getcourse.ru/fileservice/file/download/a/562611/sc/409/h/efc8ecc64ed425e98d6cde4e4585b16f.zip"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={{color: "inherit" }}
+                        >
+                            📥 Скачать ZIP файл
+                        </a>
+                    </Cell>
                 </Section>
             </List>
         </AppRoot>
