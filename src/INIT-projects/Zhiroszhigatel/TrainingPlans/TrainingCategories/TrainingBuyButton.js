d6b23d9e7f76d64c8c9ea31add1679c1b4e905bc
ilyakudrenko <ilya.kudrenko@gmail.com>
2025-01-30 19:37:47 -0600

    fixing navigation

--- src/INIT-projects/Zhiroszhigatel/TrainingPlans/TrainingCategories/TrainingBuyButton.js
+++ src/INIT-projects/Zhiroszhigatel/TrainingPlans/TrainingCategories/TrainingBuyButton.js
@@ -45,7 +45,7 @@ const INITTrainingBuyButton = ({ title, description, trainingId, price }) => {
             // setIsGreen(true); // Успешно добавлено
             setSnackbarVisible(true);
             setTimeout(async () => {
-                navigate("/trainingnavigation");
+                navigate("/");
             },2000)
 
         } catch (error) {
